c5ac2be7f1620905e2b6e698313ba45a1384316f
balajivinoth <146202696+balajivinoth@users.noreply.github.com>
2024-08-04 03:24:30 +0530

    Add files via upload

---
+++ BMICalculatorApp/app/src/main/java/com/example/mybmicalculatorapplication/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.example.mybmicalculatorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Intent ih = new Intent(Splashscreen.this, MainActivity.class);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(ih);
+                finish();
+            }
+        }, 3000);
+    }
+}
